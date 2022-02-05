2c838316522c0a8505b716e92bab052270898eb4
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-02-05 16:52:17 +0530

    nav design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import './Nav.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import logo from "../media/logo.svg";
 
 const Nav = () => {
   return (
     <nav>
+        <div className="logo">
+            <img src={logo} alt="logo" />
+        </div>
       <ul>
         <li>
           <NavLink to="/">00 home</NavLink>
         </li>
         <li>
-          <NavLink to="/">01 destination</NavLink>
+          <NavLink to="/destination">01 destination</NavLink>
         </li>
         <li>
-          <NavLink to="/">02 crew</NavLink>
+          <NavLink to="/crew">02 crew</NavLink>
         </li>
         <li>
-          <NavLink to="/">03 technology</NavLink>
+          <NavLink to="/technology">03 technology</NavLink>
         </li>
       </ul>
     </nav>
