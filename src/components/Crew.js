d6749dc7d7860120f9eb505e5a815f0f3ce8bc61
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-02-10 11:27:52 +0530

    .

--- src/components/Crew.js
+++ src/components/Crew.js
@@ -3,7 +3,14 @@ import React from "react";
 const Crew = () => {
   return (
     <div className="Crew">
-        <h1>Crewwwwwwwwwwwwwwwwwwwwwwwwww</h1>
+        <h1>Meet Your Crew!</h1>
+        <div className="crew-card">
+          <h2>Victor Glover</h2>
+          <h3 className="position">(pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station)</h3>
+          <p>
+          Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. 
+          </p>
+        </div>
     </div>
   );
 };
